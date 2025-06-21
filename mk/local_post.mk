3b5df356e808792b76fe14c101fc7bb056fd83fe
Jonas Eschmann <jonas.eschmann@gmail.com>
2025-06-21 16:52:38 -0400

    moving to OOT

--- mk/local_post.mk
+++ mk/local_post.mk
@@ -1,7 +1,10 @@
+EXTERNAL_SRC_DIR := $(ROOT_DIR)/../src
+VPATH := $(VPATH):$(EXTERNAL_SRC_DIR)
+
 CXX_SRC = \
     rl_tools/policy.cpp
 TARGET_OBJS += $(addsuffix .o,$(addprefix $(TARGET_OBJ_DIR)/,$(basename $(CXX_SRC))))
-CXXFLAGS      = $(filter-out -std=gnu17,$(CFLAGS)) -fno-rtti -fno-exceptions -std=c++17 -I $(RL_TOOLS_ROOT)
+CXXFLAGS      = $(filter-out -std=gnu17,$(CFLAGS)) -fno-rtti -fno-exceptions -std=c++17 -I $(RL_TOOLS_ROOT)/include -I $(EXTERNAL_SRC_DIR)
 
 $(TARGET_OBJ_DIR)/%.o: %.cpp
 	$(V1) mkdir -p $(dir $@)
--- src/main/rl_tools/policy.h
+++
@@ -1,9 +0,0 @@
-
-#ifdef __cplusplus
-extern "C" {
-#endif
-    void rl_tools_test(void);
-
-#ifdef __cplusplus
-}
-#endif
